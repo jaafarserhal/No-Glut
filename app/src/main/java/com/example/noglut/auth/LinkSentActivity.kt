3c253c0855f9c23a8a084ee9203f8b95521d612e
jaafarserhal <jaafarserhal@hotmail.com>
2025-06-22 17:38:06 +0300

    add send link screen

--- app/src/main/java/com/example/noglut/auth/LinkSentActivity.kt
+++ app/src/main/java/com/example/noglut/auth/LinkSentActivity.kt
@@ -1,30 +1,21 @@
 package com.example.noglut.auth
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.noglut.BaseActivity
 import com.example.noglut.R
-import com.example.noglut.databinding.ActivityResetPasswordBinding
 
-class ResetPasswordActivity : BaseActivity(){
-    private lateinit var binding: ActivityResetPasswordBinding
+class LinkSentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityResetPasswordBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_link_sent)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        setupToolbar()
-        binding.buttonResetPassword.setOnClickListener {
-            val intent = Intent(this, LinkSentActivity::class.java)
-            startActivity(intent)
         }
     }
 }
